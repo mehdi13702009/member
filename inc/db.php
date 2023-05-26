7b404f38213dfe678a9bb1c54c983cc7767d9ca7
mehdi13702009 <mehdi_13702009@yahoo.com>
2023-05-27 00:01:17 +0430

    add

---
+++ inc/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="member";
+$db = new mysqli($servername, $username, $password, $dbname);
+
+if ($db->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+  ?>
+
+?>
---
+++ menu.php
@@ -0,0 +1,27 @@
+<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<link rel="stylesheet" href="style1.css">
+
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">خانه</a>
+ 
+  <a href="#contact">لیست کاربران</a>
+  <a href="#about">خروج</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+
+</div>
+
+<h1>آقا  یا خانم .... خوش آمدید</h1>
